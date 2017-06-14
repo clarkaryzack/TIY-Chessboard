227d5ffca1421ec725cf18de13fd5988eccbeae4
clarkaryzack <clarkaryzack@gmail.com>
2017-06-14 15:09:02 -0400

    mvp

--- main.js
+++ main.js
@@ -1,21 +1,281 @@
 var A8 = document.getElementById("A8");
+var B8 = document.getElementById("B8");
+var C8 = document.getElementById("C8");
+var D8 = document.getElementById("D8");
+var E8 = document.getElementById("E8");
+var F8 = document.getElementById("F8");
+var G8 = document.getElementById("G8");
+var H8 = document.getElementById("H8");
+var A7 = document.getElementById("A7");
+var B7 = document.getElementById("B7");
+var C7 = document.getElementById("C7");
+var D7 = document.getElementById("D7");
+var E7 = document.getElementById("E7");
+var F7 = document.getElementById("F7");
+var G7 = document.getElementById("G7");
+var H7 = document.getElementById("H7");
+var A6 = document.getElementById("A6");
+var B6 = document.getElementById("B6");
+var C6 = document.getElementById("C6");
+var D6 = document.getElementById("D6");
+var E6 = document.getElementById("E6");
+var F6 = document.getElementById("F6");
+var G6 = document.getElementById("G6");
+var H6 = document.getElementById("H6");
+var A5 = document.getElementById("A5");
+var B5 = document.getElementById("B5");
+var C5 = document.getElementById("C5");
+var D5 = document.getElementById("D5");
+var E5 = document.getElementById("E5");
+var F5 = document.getElementById("F5");
+var G5 = document.getElementById("G5");
+var H5 = document.getElementById("H5");
+var A4 = document.getElementById("A4");
+var B4 = document.getElementById("B4");
+var C4 = document.getElementById("C4");
+var D4 = document.getElementById("D4");
+var E4 = document.getElementById("E4");
+var F4 = document.getElementById("F4");
+var G4 = document.getElementById("G4");
+var H4 = document.getElementById("H4");
+var A3 = document.getElementById("A3");
+var B3 = document.getElementById("B3");
+var C3 = document.getElementById("C3");
+var D3 = document.getElementById("D3");
+var E3 = document.getElementById("E3");
+var F3 = document.getElementById("F3");
+var G3 = document.getElementById("G3");
+var H3 = document.getElementById("H3");
+var A2 = document.getElementById("A2");
+var B2 = document.getElementById("B2");
+var C2 = document.getElementById("C2");
+var D2 = document.getElementById("D2");
+var E2 = document.getElementById("E2");
+var F2 = document.getElementById("F2");
+var G2 = document.getElementById("G2");
+var H2 = document.getElementById("H2");
+var A1 = document.getElementById("A1");
+var B1 = document.getElementById("B1");
+var C1 = document.getElementById("C1");
+var D1 = document.getElementById("D1");
+var E1 = document.getElementById("E1");
+var F1 = document.getElementById("F1");
+var G1 = document.getElementById("G1");
 var H1 = document.getElementById("H1");
+
 var playpause = document.getElementById("playpause")
+var stepforward = document.getElementById("stepforward")
+var stepback = document.getElementById("stepback")
 
-var bb = document.createElement("img");
-bb.id = "bb";
-bb.src = "chesspieces/bb.png";
+var bb1 = document.createElement("img");
+bb1.id = "bb1";
+bb1.src = "chesspieces/bb.png";
 
-(function blackbishop() {
-	A8.appendChild(bb);
+var bb2 = document.createElement("img");
+bb2.id = "bb2";
+bb2.src = "chesspieces/bb.png";
+
+var bn1 = document.createElement("img");
+bn1.id = "bn1";
+bn1.src = "chesspieces/bn.png";
+
+var bn2 = document.createElement("img");
+bn2.id = "bn2";
+bn2.src = "chesspieces/bn.png";
+
+var br1 = document.createElement("img");
+br1.id = "br1";
+br1.src = "chesspieces/br.png";
+
+var br2 = document.createElement("img");
+br2.id = "br2";
+br2.src = "chesspieces/br.png";
+
+var bk = document.createElement("img");
+bk.id = "bk";
+bk.src = "chesspieces/bk.png";
+
+var bq = document.createElement("img");
+bq.id = "bq";
+bq.src = "chesspieces/bq.png";
+
+var bp1 = document.createElement("img");
+bp1.id = "bp1";
+bp1.src = "chesspieces/bp.png";
+
+var bp2 = document.createElement("img");
+bp2.id = "bp2";
+bp2.src = "chesspieces/bp.png";
+
+var bp3 = document.createElement("img");
+bp3.id = "bp3";
+bp3.src = "chesspieces/bp.png";
+
+var bp4 = document.createElement("img");
+bp4.id = "bp4";
+bp4.src = "chesspieces/bp.png";
+
+var bp5 = document.createElement("img");
+bp5.id = "bp5";
+bp5.src = "chesspieces/bp.png";
+
+var bp5 = document.createElement("img");
+bp5.id = "bp5";
+bp5.src = "chesspieces/bp.png";
+
+var bp6 = document.createElement("img");
+bp6.id = "bp6";
+bp6.src = "chesspieces/bp.png";
+
+var bp7 = document.createElement("img");
+bp7.id = "bp7";
+bp7.src = "chesspieces/bp.png";
+
+var bp8 = document.createElement("img");
+bp8.id = "bp8";
+bp8.src = "chesspieces/bp.png";
+
+var wb1 = document.createElement("img");
+wb1.id = "wb1";
+wb1.src = "chesspieces/wb.png";
+
+var wb2 = document.createElement("img");
+wb2.id = "wb2";
+wb2.src = "chesspieces/wb.png";
+
+var wn1 = document.createElement("img");
+wn1.id = "wn1";
+wn1.src = "chesspieces/wn.png";
+
+var wn2 = document.createElement("img");
+wn2.id = "wn2";
+wn2.src = "chesspieces/wn.png";
+
+var wr1 = document.createElement("img");
+wr1.id = "wr1";
+wr1.src = "chesspieces/wr.png";
+
+var wr2 = document.createElement("img");
+wr2.id = "wr2";
+wr2.src = "chesspieces/wr.png";
+
+var wk = document.createElement("img");
+wk.id = "wk";
+wk.src = "chesspieces/wk.png";
+
+var wq = document.createElement("img");
+wq.id = "wq";
+wq.src = "chesspieces/wq.png";
+
+var wp1 = document.createElement("img");
+wp1.id = "wp1";
+wp1.src = "chesspieces/wp.png";
+
+var wp2 = document.createElement("img");
+wp2.id = "wp2";
+wp2.src = "chesspieces/wp.png";
+
+var wp3 = document.createElement("img");
+wp3.id = "wp3";
+wp3.src = "chesspieces/wp.png";
+
+var wp4 = document.createElement("img");
+wp4.id = "wp4";
+wp4.src = "chesspieces/wp.png";
+
+var wp5 = document.createElement("img");
+wp5.id = "wp5";
+wp5.src = "chesspieces/wp.png";
+
+var wp5 = document.createElement("img");
+wp5.id = "wp5";
+wp5.src = "chesspieces/wp.png";
+
+var wp6 = document.createElement("img");
+wp6.id = "wp6";
+wp6.src = "chesspieces/wp.png";
+
+var wp7 = document.createElement("img");
+wp7.id = "wp7";
+wp7.src = "chesspieces/wp.png";
+
+var wp8 = document.createElement("img");
+wp8.id = "wp8";
+wp8.src = "chesspieces/wp.png";
+
+
+
+
+// Object.assign(bp1,bp2,bp3,bp4,bp5,bp6,bp7,bp8)
+
+var C8piece;
+var F8piece;
+
+(function boardsetup() {
+	A8.appendChild(br1);
+	B8.appendChild(bn1);
+	C8.appendChild(bb1);
+	D8.appendChild(bq);
+	E8.appendChild(bk);
+	F8.appendChild(bb2);
+	G8.appendChild(bn2);
+	H8.appendChild(br2);
+	A7.appendChild(bp1);
+	B7.appendChild(bp2);
+	C7.appendChild(bp3);
+	D7.appendChild(bp4);
+	E7.appendChild(bp5);
+	F7.appendChild(bp6);
+	G7.appendChild(bp7);
+	H7.appendChild(bp8);
+	A2.appendChild(wp1);
+	B2.appendChild(wp2);
+	C2.appendChild(wp3);
+	D2.appendChild(wp4);
+	E2.appendChild(wp5);
+	F2.appendChild(wp6);
+	G2.appendChild(wp7);
+	H2.appendChild(wp8);
+	A1.appendChild(wr1);
+	B1.appendChild(wn1);
+	C1.appendChild(wb1);
+	D1.appendChild(wq);
+	E1.appendChild(wk);
+	F1.appendChild(wb2);
+	G1.appendChild(wn2);
+	H1.appendChild(wr2);
 })();
 
 function movepiece(piece,tospace) {
+	if(tospace.hasChildNodes()===true) tospace.removeChild(tospace.childNodes[0]);
 	tospace.appendChild(piece);
 }
 
-playpause.addEventListener("click", function() {
-	movepiece(bb,H1)
-});
+var x = 0
 
-// playpause.onClick(movepiece(bb,H1));
+stepforward.addEventListener("click", function() {
+	x+=1
+	if (x>9) x=9
+	if (x===1) {movepiece(wp4,D4)}
+	if (x===2) {movepiece(bn2,F6)}
+	if (x===3) {movepiece(wp3,C4)}
+	if (x===4) {movepiece(bp5,E6)}
+	if (x===5) {movepiece(wp7,G3)}
+	if (x===6) {movepiece(bp4,D5)}
+	if (x===7) {movepiece(wb2,G2)}
+	if (x===8) {movepiece(bb2,E7)}
+	if (x===9) {movepiece(wn2,F3)}
+});
+stepback.addEventListener("click", function() {
+	x-=1
+	if (x<0) x=0
+	if (x===8) {movepiece(wn2,G1)}
+	if (x===7) {movepiece(bb2,F8)}
+	if (x===6) {movepiece(wb2,F1)}
+	if (x===5) {movepiece(bp4,D7)}
+	if (x===4) {movepiece(wp7,G2)}
+	if (x===3) {movepiece(bp5,E7)}
+	if (x===2) {movepiece(wp3,C2)}
+	if (x===1) {movepiece(bn2,G8)}
+	if (x===0) {movepiece(wp4,D2)}
+});
